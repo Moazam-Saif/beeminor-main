4b8aeebfc19ad90bfe78569951ea5a26bb1b32e7
Moazam-Saif <msaif.bese23seecs@seecs.edu.pk>
2025-12-05 05:06:32 +0500

    ...

--- metro.config.js
+++ metro.config.js
@@ -5,9 +5,18 @@ const path = require('path');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Add support for @ alias
-config.resolver.extraNodeModules = {
-  '@': __dirname,
-};
+// Add support for @ alias - proper resolution with Proxy
+config.resolver.extraNodeModules = new Proxy(
+  {},
+  {
+    get: (target, name) => {
+      if (name === '@') {
+        return path.resolve(__dirname);
+      }
+      // Default behavior for other modules
+      return path.join(__dirname, `node_modules/${name}`);
+    },
+  }
+);
 
 module.exports = config;
